be14f3bc5b6262bfc577e37cb5934e3e2f49992f
EC2 Default User <ec2-user@ip-172-31-96-128.ec2.internal>
2020-05-20 16:04:11 +0000

    day 3

---
+++ lec3.py
@@ -0,0 +1,35 @@
+"""
+lecture notes for third day
+"""
+my_list = [1,2,3,4,5]
+#my_letters = ['a','b','c']
+my_list[0] = 6
+
+
+#print(my_list)
+#print(len(my_list))
+
+#print('6' in my_list)
+
+#my_list.sort()
+#print(my_list)
+
+#my_list.reverse()
+#print(my_list)
+
+#x,y,z = ['a','b','c']
+#print(x)
+#print(y)
+#print(z)
+
+#my_nested_list = [1,2,3,my_list]
+#print (my_nested_list)
+
+#print('helloworld')
+#print('hello world'[-1])
+
+my_letters = ['a','a','b','b','c']
+print(my_letters)
+
+my_unique_items = set(my_letters)
+print('a' in my_unique_items)
